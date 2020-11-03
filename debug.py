2965221276a27d54d90e0cc56e95a0016ca083cd
Suho Lee <susemeee@gmail.com>
2020-11-03 14:52:43 +0900

    Update debug code

--- debug.py
+++ debug.py
@@ -1,18 +1,31 @@
 
+from sklearn.metrics import f1_score
+
 def debug(raw_test_data, load_data, predictions):
     gt_data = load_data('gt.tsv', delimiter='\t', names=['label', 'message'], header=None)
 
+    gts = []
+    preds = []
     is_corrects = []
+
     for i, message in enumerate(raw_test_data):
         row = gt_data[gt_data['message'] == raw_test_data[i]]
-        is_spam = 'spam' if predictions[i] == 1 else 'ham'
 
         if len(row) == 0:
             print(message)
             continue
 
-        is_correct = row['label'].tolist()[0] == is_spam
+        is_spam = 1 if row['label'].tolist()[0] == 'spam' else 0
+        is_correct = predictions[i] == is_spam
 
         is_corrects.append(is_correct)
+        gts.append(is_spam)
+        preds.append(predictions[i])
 
     print('P: {}%'.format(is_corrects.count(True) / len(is_corrects)))
+    print('F1: {}%'.format(get_f1(gts, preds)))
+
+
+def get_f1(gt, pred):
+    score = f1_score(gt, pred)
+    return score
